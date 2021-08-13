88fca899b0345f55d0bc40fa8187d9df30fdd732
Ty Edge <tyedge.inc@gmail.com>
2021-08-13 03:28:08 +0000

    Adding Task 6 file

---
+++ supervised_learning/0x02-tensorflow/6-train.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+"""This module contains a function that builds, trains, and saves a neural
+network classifier"""
+
+import tensorflow as tf
+calculate_accuracy = __import__('3-calculate_accuracy').calculate_accuracy
+calculate_loss = __import__('4-calculate_loss').calculate_loss
+create_placeholders = __import__('0-create_placeholders').create_placeholders
+create_train_op = __import__('5-create_train_op').create_train_op
+forward_prop = __import__('2-forward_prop').forward_prop
+
+
+def train(X_train, Y_train, X_valid, Y_valid, layer_sizes, activations,
+          alpha, iterations, save_path="/tmp/model.ckpt"):
+    """This function builds, trains, and saves a neural network classifier"""
+
+    xtra = X_train
+    tray = Y_train
+    valx = X_valid
+    valy = Y_valid
+    lays = layer_sizes
+    reps = iterations
+    rpt = 100
+    sp = save_path
+
+    x, y = create_placeholders(xtra.shape[1], tray.shape[1])
+    pred = forward_prop(x, lays, activations)
+    loss = calculate_loss(y, pred)
+    acc = calculate_accuracy(y, pred)
+    top = create_train_op(loss, alpha)
+
+    tf.add_to_collection('x', x)
+    tf.add_to_collection('y', y)
+    tf.add_to_collection('y_ pred', pred)
+    tf.add_to_collection('loss', loss)
+    tf.add_to_collection('accuracy', acc)
+    tf.add_to_collection('train_op', top)
+
+    init = tf.global_variables_initializer()
+    savenn = tf.train.Saver()
+
+    with tf.Session() as sess:
+        sess.run(init)
+        for i in range(reps + 1):
+            tacc, losst = sess.run([acc, loss], {x: xtra, y: tray})
+            vacc, vloss = sess.run([acc, loss], {x: valx, y: valy})
+            if i % rpt == 0 or i == reps:
+                print("After {} iterations:".format(i))
+                print("\tTraining Cost: {}".format(losst))
+                print("\tTraining Accuracy: {}".format(tacc))
+                print("\tValidation Cost: {}".format(vloss))
+                print("\tValidation Accuracy: {}".format(vacc))
+            sess.run(top, {x: xtra, y: tray})
+    return savenn.save(sess, sp)

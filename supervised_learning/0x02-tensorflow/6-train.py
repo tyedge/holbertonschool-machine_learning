2fed406d2bdf22391d7a12c11033a5bc3d039d4d
Ty Edge <tyedge.inc@gmail.com>
2021-08-13 05:38:05 +0000

    Changed variable names

--- supervised_learning/0x02-tensorflow/6-train.py
+++ supervised_learning/0x02-tensorflow/6-train.py
@@ -14,42 +14,42 @@ def train(X_train, Y_train, X_valid, Y_valid, layer_sizes, activations,
           alpha, iterations, save_path="/tmp/model.ckpt"):
     """This function builds, trains, and saves a neural network classifier"""
 
-    xtra = X_train
+    '''xtra = X_train
     tray = Y_train
     valx = X_valid
     valy = Y_valid
     lays = layer_sizes
     reps = iterations
     rpt = 100
-    sp = save_path
+    sp = save_path'''
 
-    x, y = create_placeholders(xtra.shape[1], tray.shape[1])
-    pred = forward_prop(x, lays, activations)
-    loss = calculate_loss(y, pred)
-    acc = calculate_accuracy(y, pred)
-    top = create_train_op(loss, alpha)
+    x, y = create_placeholders(X_train.shape[1], Y_train.shape[1])
+    y_pred = forward_prop(x, lays, activations)
+    loss = calculate_loss(y, y_pred)
+    accuracy = calculate_accuracy(y, y_pred)
+    train_op = create_train_op(loss, alpha)
 
     tf.add_to_collection('x', x)
     tf.add_to_collection('y', y)
-    tf.add_to_collection('y_pred', pred)
+    tf.add_to_collection('y_pred', y_pred)
     tf.add_to_collection('loss', loss)
-    tf.add_to_collection('accuracy', acc)
-    tf.add_to_collection('train_op', top)
+    tf.add_to_collection('accuracy', accuracy)
+    tf.add_to_collection('train_op', train_op)
 
     init = tf.global_variables_initializer()
     saver = tf.train.Saver()
 
     with tf.Session() as sess:
         sess.run(init)
-        for i in range(reps + 1):
-            tacc, losst = sess.run([acc, loss], {x: xtra, y: tray})
-            vacc, vloss = sess.run([acc, loss], {x: valx, y: valy})
-            if i % rpt == 0 or i == reps:
+        for i in range(iterations + 1):
+            tacc, losst = sess.run([accuracy, loss], {x: X_train, y: Y_train})
+            vacc, vloss = sess.run([accuracy, loss], {x: X_valid, y: Y_valid})
+            if i % 100 == 0 or i == iterations:
                 print("After {} iterations:".format(i))
                 print("\tTraining Cost: {}".format(losst))
                 print("\tTraining Accuracy: {}".format(tacc))
                 print("\tValidation Cost: {}".format(vloss))
                 print("\tValidation Accuracy: {}".format(vacc))
-            if i < reps:
-                sess.run(top, {x: xtra, y: tray})
-    return saver.save(sess, sp)
+            if i < iterations:
+                sess.run(train_op, {x: X_train, y: Y_train})
+    return saver.save(sess, save_path)

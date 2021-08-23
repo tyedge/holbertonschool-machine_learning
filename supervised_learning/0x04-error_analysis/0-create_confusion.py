2ec1de069cf68088957c4d93eb36027390cf128b
Ty Edge <tyedge.inc@gmail.com>
2021-08-23 21:56:40 +0000

    Removed extraneous print function

--- supervised_learning/0x04-error_analysis/0-create_confusion.py
+++ supervised_learning/0x04-error_analysis/0-create_confusion.py
@@ -6,6 +6,5 @@ import numpy as np
 
 def create_confusion_matrix(labels, logits):
     """This function creates a confusion matrix"""
-    print("The shape of the labels is {}".format(labels.shape))
     out = np.matmul(labels.T, logits)
     return out

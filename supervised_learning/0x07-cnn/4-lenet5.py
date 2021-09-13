c9b45b974fb34cc41dc7614997f3285a465712a5
Ty Edge <tyedge.inc@gmail.com>
2021-09-13 02:53:04 +0000

    Corrected error in fully connected layer

--- supervised_learning/0x07-cnn/4-lenet5.py
+++ supervised_learning/0x07-cnn/4-lenet5.py
@@ -10,18 +10,36 @@ def lenet5(x, y):
 using tensorflow"""
     init = tf.contrib.layers.variance_scaling_initializer()
     relu = tf.nn.relu
-    conv1 = tf.layers.Conv2D(filters=6, kernel_size=(5, 5), padding="same",
-                             activation=relu, kernel_initializer=init)(x)
+
+    conv1 = tf.layers.Conv2D(filters=6,
+                             kernel_size=(5, 5),
+                             padding="same",
+                             activation=relu,
+                             kernel_initializer=init)(x)
+
     pool1 = tf.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(conv1)
-    conv2 = tf.layers.Conv2D(filters=16, kernel_size=(5, 5), padding="valid",
-                             activation=relu, kernel_initializer=init)(pool1)
+
+    conv2 = tf.layers.Conv2D(filters=16,
+                             kernel_size=(5, 5),
+                             padding="valid",
+                             activation=relu,
+                             kernel_initializer=init)(pool1)
+
     pool2 = tf.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(conv2)
+
     flat = tf.layers.Flatten()(pool2)
-    full1 = tf.layers.Dense(units=120, activation=relu,
+
+    full1 = tf.layers.Dense(units=120,
+                            activation=relu,
                             kernel_initializer=init)(flat)
-    full2 = tf.layers.Dense(units=120, activation=relu,
+
+    full2 = tf.layers.Dense(units=84,
+                            activation=relu,
                             kernel_initializer=init)(full1)
-    fullout = tf.layers.Dense(units=10, kernel_initializer=init)(full2)
+
+    fullout = tf.layers.Dense(units=10,
+                              kernel_initializer=init)(full2)
+
     softmx = tf.nn.softmax(fullout)
     loss = tf.losses.softmax_cross_entropy(y, fullout)
     train = tf.train.AdamOptimizer().minimize(loss)

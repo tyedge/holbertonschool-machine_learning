6163d986b9140377181622bb6c3564fd5ae4cf4c
Ty Edge <tyedge.inc@gmail.com>
2021-10-08 23:50:09 +0000

    fixed function definition & reworked function

--- supervised_learning/0x06-keras/8-train.py
+++ supervised_learning/0x06-keras/8-train.py
@@ -10,16 +10,17 @@ import tensorflow.keras as K
 def train_model(network, data, labels, batch_size, epochs,
                 validation_data=None, early_stopping=False, patience=0,
                 learning_rate_decay=False, alpha=0.1, decay_rate=1,
-                verbose=True, shuffle=False):
+                save_best=False, filepath=None, verbose=True, shuffle=False):
     """This function trains a model using mini-batch gradient descent,
 analyzes validaiton data, trains the model using early stopping, trains
 the model with learning rate decay, and saves the best iteration of the
 model"""
-
+    callbcks = []
     if validation_data and early_stopping:
-        callbcks = [K.callbacks.EarlyStopping(monitor='val_loss',
-                                              patience=patience,
-                                              mode="min")]
+
+        callbcks.append(K.callbacks.EarlyStopping(monitor='val_loss',
+                                                  patience=patience,
+                                                  mode="min"))
     if validation_data and learning_rate_decay:
 
         def scheduler(epoch):
@@ -27,13 +28,11 @@ model"""
             learning rate"""
             return alpha / (1 + decay_rate * epoch)
 
-        callbcks = [K.callbacks.LearningRateScheduler(scheduler, verbose=1)]
-
+        callbcks.append(K.callbacks.LearningRateScheduler(scheduler,
+                                                          verbose=1))
     if save_best:
-        callbcks = [K.callbacks.ModelCheckpoint(filepath, save_best_only=True)]
-
-    else:
-        callbcks = None
+        callbcks.append(K.callbacks.ModelCheckpoint(filepath,
+                                                    save_best_only=True))
 
     return network.fit(x=data, y=labels, batch_size=batch_size, epochs=epochs,
                        verbose=verbose, callbacks=callbcks,

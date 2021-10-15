e45e88cdf1228b4c2b77146218848724ff4a5766
Ty Edge <tyedge.inc@gmail.com>
2021-10-15 02:29:52 +0000

    Trying again

--- supervised_learning/0x01-classification/26-deep_neural_network.py
+++ supervised_learning/0x01-classification/26-deep_neural_network.py
@@ -121,8 +121,8 @@ attributes __weights and __cache"""
 
     def save(self, filename):
         """This method saves the instance object to a file in pickle format"""
-        with open(filename, "wb") as f:
-            Pickler(f).dump(self)
+        with open(str(filename), 'wb') as f:
+            return pickle.dump(self, f)
 
     @staticmethod
     def load(filename):

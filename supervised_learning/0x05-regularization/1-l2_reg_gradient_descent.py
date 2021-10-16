7579d3a2335b616a9ef6f1efd92d3b33abff73b5
Ty Edge <tyedge.inc@gmail.com>
2021-10-16 13:37:09 +0000

    Adding Task 1 file

--- supervised_learning/0x05-regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/0x05-regularization/1-l2_reg_gradient_descent.py
@@ -8,18 +8,22 @@ import numpy as np
 def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
     """This function updates the weights and biases of a neural network using
 gradient descent with L2 regularization"""
-
     sz = Y.shape[1]
-    diff = cache["A{}".format(L)] - Y
-    for i in range(L, 0, -1):
-        nm = (lambtha / sz) * weights["W{}".format(i)]
-        dub = ((1 / sz) * np.matmul(diff, cache["A{}".format(i - 1)].T)) + nm
-
-        bee = (1 / sz) * np.sum(diff, axis=1, keepdims=True)
-
-        weights["W{}".format(i)] -= (alpha * dub)
-        weights["b{}".format(i)] -= (alpha * bee)
-
-        d1 = np.matmul(weights["W{}".format(i)].T, diff)
-        d2 = 1 - np.square(cache["A{}".format(i - 1)])
-        diff = d1 * d2
+    cpy = weights.copy()
+    for i in reversed(range(L)):
+        w = "W{}".format(i + 1)
+        b = "b{}".format(i + 1)
+        a1 = "A{}".format(i + 1)
+        a2 = "A{}".format(i)
+        nrm = (lambtha / sz) * cpy[w]
+        if i == (L - 1):
+            diff = cache[a1] - Y
+            dub = (np.matmul(cache[a2], diff.T) / sz).T
+        else:
+            d1 = np.matmul(cpy["W{}".format(i + 2)].T, diff)
+            d2 = 1 - cache[a1] ** 2
+            diff = d1 * d2
+        dub = np.matmul(diff, cache[a2].T) / sz + nrm
+        bee = np.sum(diff, axis=1, keepdims=True) / sz
+        weights[w] -= (alpha * dub)
+        weights[b] -= (alpha * bee)

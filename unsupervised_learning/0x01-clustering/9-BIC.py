1e5999d18b877e32eb10d666f3eb8d94fd7d3d01
Ty Edge <tyedge.inc@gmail.com>
2021-11-05 06:03:37 +0000

    Adding revised Task 9 file

--- unsupervised_learning/0x01-clustering/9-BIC.py
+++ unsupervised_learning/0x01-clustering/9-BIC.py
@@ -11,17 +11,19 @@ def BIC(X, kmin=1, kmax=None, iterations=1000, tol=1e-5, verbose=False):
 the Bayesian Information Criterion"""
     if type(X) is not np.ndarray or len(X.shape) != 2:
         return None, None, None, None
-    if type(kmin) is not int or kmin <= 0:
-        return None, None, None, None
     if kmax is None:
         kmax = X.shape[0]
-    if type(kmax) is not int or kmax <= 0:
+    if kmin >= X.shape[0] or kmax > X.shape[0]:
+        return None, None, None, None
+    if type(kmin) is not int or kmin < 1:
+        return None, None, None, None
+    if type(kmax) is not int or kmax < 1:
         return None, None, None, None
     if kmin >= kmax:
         return None, None, None, None
     if type(iterations) is not int or iterations <= 0:
         return None, None, None, None
-    if type(tol) is not float or tol < 0:
+    if type(tol) is not float or tol <= 0:
         return None, None, None, None
     if type(verbose) is not bool:
         return None, None, None, None
